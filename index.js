629704e22404032936e27f357172c916c36eb826
Agustin Vera <ronedev25@gmail.com>
2023-01-19 16:03:49 -0300

    add: routes of: tasks and users with their methods

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 // Importing module
 const express_1 = __importDefault(require("express"));
+const client_1 = require("@prisma/client");
 const app = (0, express_1.default)();
-const PORT = 3000;
+const PORT = 3001;
+const prisma = new client_1.PrismaClient();
 // Handling GET / Request
-app.get('/', (req, res) => {
-    res.send('Welcome to todo-proyect backend!');
+app.get("/", (req, res) => {
+    res.send("Welcome to todo-proyect backend!");
 });
+//Getting all users and their tasks
+app.get("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    yield prisma.$connect();
+    const allUsers = yield prisma.user.findMany({
+        include: {
+            tasks: true,
+        },
+    });
+    res.json(allUsers);
+}));
+//Getting one user and their tasks
+app.get("/users/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    yield prisma.$connect();
+    const user = yield prisma.user.findUnique({
+        where: { id },
+        include: {
+            tasks: true,
+        },
+    });
+    res.json(user);
+}));
+//Getting all tasks
+app.get("/tasks", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    yield prisma.$connect();
+    const allTasks = yield prisma.task.findMany();
+    res.json(allTasks);
+}));
+//Getting all tasks from a user
+app.get("/tasks/:userId", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.params;
+    yield prisma.$connect();
+    const allTasks = yield prisma.task.findMany({ where: { id: userId } });
+    res.json(allTasks);
+}));
 // Server setup
 app.listen(PORT, () => {
-    console.log('The application is listening '
-        + 'on port http://localhost:' + PORT);
+    console.log("The application is listening " + "on port http://localhost:" + PORT);
 });
--- index.ts
+++ index.ts
@@ -3,12 +3,53 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
-const PORT: Number = 3000;
+const PORT: Number = 3001;
+
+const prisma = new PrismaClient()
 
 // Handling GET / Request
 app.get("/", (req, res) => {
   res.send("Welcome to todo-proyect backend!");
 });
+
+//Getting all users and their tasks
+app.get("/users", async (req ,res)=>{
+    await prisma.$connect()
+    const allUsers = await prisma.user.findMany({
+        include: {
+          tasks: true,
+        },
+      })
+    res.json(allUsers)
+})
+
+//Getting one user and their tasks
+app.get("/users/:id", async (req ,res)=>{
+    const {id} = req.params
+    await prisma.$connect()
+    const user = await prisma.user.findUnique({
+        where: {id},
+        include: {
+          tasks: true,
+        },
+      })
+    res.json(user)
+})
+
+//Getting all tasks
+app.get("/tasks", async (req ,res)=>{
+    await prisma.$connect()
+    const allTasks = await prisma.task.findMany()
+    res.json(allTasks)
+})
+
+//Getting all tasks from a user
+app.get("/tasks/:userId", async (req ,res)=>{
+    const {userId} = req.params
+    await prisma.$connect()
+    const allTasks = await prisma.task.findMany({where: {id: userId}})
+    res.json(allTasks)
+})
 
 // Server setup
 app.listen(PORT, () => {
@@ -16,39 +57,3 @@ app.listen(PORT, () => {
     "The application is listening " + "on port http://localhost:" + PORT
   );
 });
-
-const prisma = new PrismaClient();
-
-async function main() {
-    await prisma.$connect()
-  
-    await prisma.user.create({
-      data: {
-        name: 'Rich',
-        email: 'hello@prisma.com',
-        tasks: {
-          create: {
-            name: 'My first task',
-            description: 'Lots of really interesting stuff',
-          },
-        },
-      },
-    })
-  
-    const allUsers = await prisma.user.findMany({
-      include: {
-        tasks: true,
-      },
-    })
-    console.dir(allUsers, { depth: null })
-  }
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
